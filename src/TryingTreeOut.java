985e6cca7cf5e0e474c7bc11072690240f415508
Gavin <gavoshmavo1@gmail.com>
2022-10-25 22:49:14 -0700

    Completely changed Tree
    changed tree so that there is some amount of functionality, but only works in the case of a single commit. When there are more than one commit, something goes wrong.

--- src/TryingTreeOut.java
+++ src/TryingTreeOut.java
@@ -16,8 +16,8 @@ public class TryingTreeOut {
 		Blob blob1 = new Blob("blob1.txt");
 		Blob blob2 = new Blob("blob2.txt");
 		Blob blob3 = new Blob("blob3.txt");
-		Blob blob4 = new Blob("blob4.txt");
-		Blob blob5 = new Blob("blob5.txt");
+//		Blob blob4 = new Blob("blob4.txt");
+//		Blob blob5 = new Blob("blob5.txt");
 		ind.add("blob1.txt");
 		ind.add("blob2.txt");
 	
@@ -29,19 +29,19 @@ public class TryingTreeOut {
 		
 		Commit com2 = new Commit("summary2", "Author2", com1);
 		com2.writesFileToObjects();
-		
-		
-		ind.add("blob4.txt");
-		ind.add("blob5.txt");
-		
-		Commit com3 = new Commit("summary3", "Author3", com2);
-		com3.writesFileToObjects();
-		
-		Commit com4 = new Commit("summary4", "Author4", com3);
-		com4.writesFileToObjects();
-		
-		Commit com5 = new Commit("summary5", "Author5", com4);
-		com5.writesFileToObjects();
+//		
+//		
+//		ind.add("blob4.txt");
+//		ind.add("blob5.txt");
+//		
+//		Commit com3 = new Commit("summary3", "Author3", com2);
+//		com3.writesFileToObjects();
+//		
+//		Commit com4 = new Commit("summary4", "Author4", com3);
+//		com4.writesFileToObjects();
+//		
+//		Commit com5 = new Commit("summary5", "Author5", com4);
+//		com5.writesFileToObjects();
 		
 	}
 
--- src/git/Tree.java
+++ src/git/Tree.java
@@ -1,107 +1,109 @@
-package git;
-
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
-
-public class Tree {
-	public String sha1;
-	public Tree (ArrayList <String> listy) throws NoSuchAlgorithmException, IOException {
-		//create string from arraylist create string with new line
-		//turn string into parameter
-		//create file from sha1
-		String fromList = listy.get(0);
-		for (int i = 1; i < listy.size(); i ++) {
-			fromList += "\n"+listy.get(i);
-		}
-		System.out.println("\n\nLIST: \n"+fromList+"\n");
-		FileWriter fOne = new FileWriter("tree.txt");//output file
-		PrintWriter printW = new PrintWriter (fOne);//writing stuff onto fw
-		printW.write(fromList);
-		if(printW != null) {
-		   printW.flush();
-		   printW.close();
-		}
-		Blob b = new Blob ("tree.txt");
-		String newFile;
-		String fileSha;
-		if(fromList.substring(0,4).equals("tree")) {
-			fileSha = fromList.substring(7);
-			newFile = b.generateSHA1Hash(fileSha);
-		}else {
-			fileSha = fromList.substring(7,fromList.substring(7).indexOf(" ")+7);
-			newFile = b.generateSHA1Hash("./objects/"+fileSha); // fromList is not a filepath -- this is why it's throwing an error
-		}
-		
-		System.out.println("TREE: " + "./objects/"+newFile);
-		sha1 = newFile;
-		FileWriter fTwo = new FileWriter(newFile);//output file
-		PrintWriter printW2 = new PrintWriter (fTwo);//writing stuff onto fw
-		printW2.write("fromList");
-		if(printW2 != null) {
-		   printW2.flush();
-		   printW2.close();
-		}
-	}
-	
-
-	
-	public String getSha1() {
-		return sha1;
-	}
-}
 //package git;
-//import java.io.BufferedWriter;
-//import java.io.File;
+//
 //import java.io.FileWriter;
 //import java.io.IOException;
 //import java.io.PrintWriter;
-//import java.nio.file.Files;
-//import java.nio.file.Paths;
-//import java.security.MessageDigest;
 //import java.security.NoSuchAlgorithmException;
-//import java.util.*;
+//import java.util.ArrayList;
 //
 //public class Tree {
-//	String treeSha1;
-//	String treeStr;
-//	ArrayList<String> arr;
-//	public Tree(ArrayList<String> arr) throws NoSuchAlgorithmException, IOException {
-//		this.arr = arr;
-//		treeStr = arrLstToStr(arr);
-//		treeSha1 = getSHA(treeStr);
-//
+//	public String sha1;
+//	public Tree (ArrayList <String> listy) throws NoSuchAlgorithmException, IOException {
+//		//create string from arraylist create string with new line
+//		//turn string into parameter
+//		//create file from sha1
+//		String fromList = listy.get(0);
+//		for (int i = 1; i < listy.size(); i ++) {
+//			fromList += "\n"+listy.get(i);
+//		}
+//		System.out.println("\n\nLIST: \n"+fromList+"\n");
+//		FileWriter fOne = new FileWriter("tree.txt");//output file
+//		PrintWriter printW = new PrintWriter (fOne);//writing stuff onto fw
+//		printW.write(fromList);
+//		if(printW != null) {
+//		   printW.flush();
+//		   printW.close();
+//		}
+//		Blob b = new Blob ("tree.txt");
+//		String newFile;
+//		String fileSha;
+//		if(fromList.substring(0,4).equals("tree")) {
+//			fileSha = fromList.substring(7);
+//			newFile = b.generateSHA1Hash(fileSha);
+//		}else {
+//			fileSha = fromList.substring(7,fromList.substring(7).indexOf(" ")+7);
+//			newFile = b.generateSHA1Hash("./objects/"+fileSha); // fromList is not a filepath -- this is why it's throwing an error
+//		}
 //		
-//		Files.deleteIfExists(Paths.get("./objects/" + treeSha1));
-//		File treeFile = new File("./objects", treeSha1);
-//		treeFile.createNewFile();
-//		BufferedWriter bw = new BufferedWriter(new FileWriter(treeFile));
-//		//FileWriter fw = new FileWriter(treeFile);
-//		bw.write(treeStr);
-//		bw.close();
-//		
+//		System.out.println("TREE: " + "./objects/"+newFile);
+//		sha1 = newFile;
+//		FileWriter fTwo = new FileWriter(newFile);//output file
+//		PrintWriter printW2 = new PrintWriter (fTwo);//writing stuff onto fw
+//		printW2.write("fromList");
+//		if(printW2 != null) {
+//		   printW2.flush();
+//		   printW2.close();
+//		}
 //	}
 //	
-//	private String arrLstToStr(ArrayList<String> array) {
-//		String arrStr = array.get(0);
-//		if(array.size()>1) {
-//			for(int i = 1; i<array.size(); i++) {
-//				arrStr += array.get(i)+"\n";
-//			}
-//		}
-//		return(arrStr);
-//	}
+//
 //	
 //	public String getSha1() {
-//		return treeSha1;
-//	}
-//	
-//	
-//	private String getSHA(String convertme) throws NoSuchAlgorithmException {
-//		byte[] bytes = convertme.getBytes();
-//	    MessageDigest md = MessageDigest.getInstance("SHA-1");
-//	    return Base64.getEncoder().encodeToString(md.digest(bytes));
+//		return sha1;
 //	}
 //}
+package git;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.*;
+
+public class Tree {
+	String treeSha1;
+	String treeStr;
+	ArrayList<String> arr;
+	public Tree(ArrayList<String> arr) throws NoSuchAlgorithmException, IOException {
+		this.arr = arr;
+		treeStr = arrLstToStr(arr);
+		treeSha1 = getSHA(treeStr);
+
+		System.out.println("str: "+treeStr + " sha: "+treeSha1);
+		Files.deleteIfExists(Paths.get("./objects/" + treeSha1));
+		File treeFile = new File("./objects", treeSha1);
+		System.out.println();
+		System.out.println(treeFile.exists());
+		treeFile.createNewFile();
+		BufferedWriter bw = new BufferedWriter(new FileWriter(treeFile));
+		//FileWriter fw = new FileWriter(treeFile);
+		bw.write(treeStr);
+		bw.close();
+		
+	}
+	
+	private String arrLstToStr(ArrayList<String> array) {
+		String arrStr = array.get(0) + "\n";
+		if(array.size()>1) {
+			for(int i = 1; i<array.size(); i++) {
+				arrStr += array.get(i)+"\n";
+			}
+		}
+		return(arrStr);
+	}
+	
+	public String getSha1() {
+		return treeSha1;
+	}
+	
+	
+	private String getSHA(String convertme) throws NoSuchAlgorithmException {
+		byte[] bytes = convertme.getBytes();
+	    MessageDigest md = MessageDigest.getInstance("SHA-1");
+	    return Base64.getEncoder().encodeToString(md.digest(bytes));
+	}
+}
